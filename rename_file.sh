54254a2cee1f1a97394a4c6676fc8c07da628fb8
nitheesh mn <mail2nitheeshmavila@gmail.com>
2016-07-29 12:06:25 +0530

    first commitwq

---
+++ rename_file.sh
@@ -0,0 +1,14 @@
+# Write a shell script which will rename all the files in the current directory whose name contains upper-c    ase characters into all lower case. For example, if the directory contains a file whose name is CoUnt.c, it     should be renamed to count.c.
+ #(hint: use a for  loop, use the "tr" command)
+
+# ls | tr 'A-Z' 'a-z'
+ # by using for loop
+
+
+for f in *;
+    do
+     if [ -f "$f" ]; then
+       tr 'A-Z' 'a-z'
+     fi
+   done
+
